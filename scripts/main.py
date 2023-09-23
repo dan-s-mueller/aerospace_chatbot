202e5fde67b6d75f37e19e2d74e00abce467b2fb
Dan Mueller <dsm@danmueller.pro>
2023-09-23 16:11:17 -0700

    Object oriented, filtered queries

--- scripts/main.py
+++ scripts/main.py
@@ -25,7 +25,7 @@ pinecone.init(
     api_key=os.getenv('PINECONE_API_KEY'),
     environment=os.getenv('PINECONE_ENVIRONMENT') 
 )
-index_name = "langchain-quickstart"
+index_name = 'langchain-quickstart'
 
 # Find all docs in data folder and import them
 current_path=os.path.dirname(os.path.abspath(__file__))
@@ -36,16 +36,38 @@ refresh=False
 if refresh:
     data_import.load_docs(index_name,embeddings_model,docs)
 
-qa=queries.qa_model(index_name,
+
+# filter_list={'source':'AMS_2006.pdf'}
+qa_model_obj=queries.QA_Model(index_name,
                     embeddings_model,
                     llm,
-                    k=8,
+                    k=6,
                     search_type='mmr',
-                    verbose=False)
+                    verbose=False,
+                    filter=None)
+
 
 query = 'What can you tell me about latch mechanism design failures which have occurred'
-# result = qa({"question": query})
-queries.query_docs(qa,query)
+qa_model_obj.query_docs(query)
 
-query_followup='Provide details on volatile spherical joint interfaces'
-queries.query_docs(qa,query_followup)
+query_followup='Which one of the sources discussed volatile spherical joint interfaces'
+
+# Response without filter
+# qa_model_obj.query_docs(query_followup)
+
+# Response with filter
+# {'page':'GPT-4 Technical Report'}
+# {"filter": {"source": ["source1", "source2"]}}
+# {"filter": {"source": ["source1", "source2"]}}
+#         search_kwargs={'filter': {'paper_title':'GPT-4 Technical Report'}}
+filter_list = list(set(item['source'] for item in qa_model_obj.sources[-1])) # Beware, if you have a typo it will run but not work (e.g. 'source:' instead of 'source')
+# Filter by multiple found items in previous response: https://docs.pinecone.io/docs/metadata-filtered-search
+filter_items=[]
+for item in filter_list:
+    filter_item={'source': item}
+    filter_items.append(filter_item)
+filter_dict={'$or':filter_items}
+
+qa_model_obj.update_model(search_type='similarity',
+                          filter=filter_dict)
+qa_model_obj.query_docs(query_followup)
--- scripts/queries.py
+++ scripts/queries.py
@@ -1,78 +1,111 @@
-'''
-@author: dsmueller3760
-Query docs and generate responses with different kinds of chains and agents
-'''
 from dotenv import load_dotenv, find_dotenv
 from langchain.vectorstores import Pinecone
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
+
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.llm import LLMChain
 from langchain import PromptTemplate
+
 from prompts import CONDENSE_QUESTION_PROMPT, QA_PROMPT, QA_WSOURCES_PROMPT
 
 
-def qa_model(index_name,
-              embeddings_model,
-              llm,
-              k=6,
-              search_type='similarity',
-              temperature=0,
-              verbose=False,
-              chain_type='stuff'):
-    
-    load_dotenv(find_dotenv(),override=True)
 
-    # Read in from the vector database
-    vectorstore = Pinecone.from_existing_index(index_name,embeddings_model)
-    
-    # Do a similarity search with the query, returning the top k docs.
-    # docs = vectorstore.similarity_search(query,k=k)
+class QA_Model:
+    def __init__(self, 
+                 index_name,
+                 embeddings_model,
+                 llm,
+                 k=6,
+                 search_type='similarity',
+                 temperature=0,
+                 verbose=False,
+                 chain_type='stuff',
+                 filter=None):
+        
+        self.index_name:str=index_name
+        self.embeddings_model:OpenAIEmbeddings=embeddings_model
+        self.llm=llm
+        self.k:int=k
+        self.search_type:str=search_type
+        self.temperature:int=temperature
+        self.verbose:bool=verbose
+        self.chain_type:str=chain_type
+        self.filter:dict=filter
 
-    llm = OpenAI(temperature=0)
+        load_dotenv(find_dotenv(),override=True)
 
-    # Generate a response from the query with the k docs which were found in the database
-    # chain = load_qa_chain(llm, chain_type='stuff', verbose=verbose)
-    # response=chain.run(input_documents=docs, question=query)
+        # Read in from the vector database
+        self.vectorstore = Pinecone.from_existing_index(index_name,embeddings_model)
 
-    question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT,verbose=verbose)
-    # doc_chain = load_qa_chain(llm, chain_type=chain_type, prompt=QA_PROMPT,verbose=verbose)
-    doc_chain = load_qa_with_sources_chain(llm, chain_type='stuff',prompt=QA_WSOURCES_PROMPT,verbose=verbose)
+        # Set up question generator and qa with sources
+        self.question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT,verbose=verbose)
+        self.doc_chain = load_qa_with_sources_chain(llm, chain_type=chain_type,prompt=QA_WSOURCES_PROMPT,verbose=verbose)
 
-    chat_history=ConversationBufferMemory(memory_key='chat_history',
-                                          input_key='question',
-                                          output_key='answer',
-                                          return_messages=True)
+        # Establish chat history
+        self.chat_history=ConversationBufferMemory(memory_key='chat_history',
+                                            input_key='question',
+                                            output_key='answer',
+                                            return_messages=True)
 
 
-    qa = ConversationalRetrievalChain(
-        retriever=vectorstore.as_retriever(search_type=search_type,
-                                            search_kwargs={'k':k,'fetch_k': 50}),  # See as_retriever docs for parameters
-                                            combine_docs_chain=doc_chain, 
-                                            question_generator=question_generator,
-                                            memory=chat_history,
-                                            return_source_documents=True,
-                                            verbose=verbose,
-                                            return_generated_question=True)
+        if search_type=='mmr':
+            search_kwargs={'k':k,'fetch_k':50,'filter':filter} # See as_retriever docs for parameters
+        else:
+            search_kwargs={'k':k,'filter':filter} # See as_retriever docs for parameters
 
-    # query = 'What can you tell me about latch mechanism design failures which have occurred'
-    # result = qa({"question": query})
+        self.qa = ConversationalRetrievalChain(
+                    retriever=self.vectorstore.as_retriever(search_type=search_type,
+                                                            search_kwargs=search_kwargs),  
+                    combine_docs_chain=self.doc_chain, 
+                    question_generator=self.question_generator,
+                    memory=self.chat_history,
+                    verbose=verbose,
+                    return_source_documents=True,
+                    return_generated_question=True)
+        
+        self.sources=[]
 
-    # query_followup='Provide details on the inadequate engineering controls on critical features'
-    # result = qa({"question": query_followup})
+    def query_docs(self,query,filter=None):
+        self.result=self.qa({"question": query})
 
-    return qa
+        print('-------------')
+        print(query+'\n')
+        print(self.result['answer']+'\n\n'+'Sources:'+'\n')
 
-def query_docs(qa,query,filter=None):
-    result=qa({"question": query})
+        temp_sources=[]
+        for data in self.result['source_documents']:
+            temp_sources.append(data.metadata)
+            print(data.metadata)
 
-    print(query+'\n')
-    print(result['answer']+'\n\n'+'Sources:'+'\n')
+        self.sources.append(temp_sources)
+        print('\nGenerated question: '+self.result['generated_question'])
+        print('-------------\n')
 
-    for data in result['source_documents']:
-        print(data.metadata)
+    def update_model(self,
+                    k=6,
+                    search_type='similarity',
+                    fetch_k=50,
+                    verbose=None,
+                    filter=None):
 
-    print('\nGenerated question: '+result['generated_question'])
-    print('-------------\n')
+        # Set up question generator and qa with sources
+        self.question_generator = LLMChain(llm=self.llm, prompt=CONDENSE_QUESTION_PROMPT,verbose=verbose)
+        self.doc_chain = load_qa_with_sources_chain(self.llm, chain_type=self.chain_type,prompt=QA_WSOURCES_PROMPT,verbose=verbose)
+
+        if search_type=='mmr':
+            search_kwargs={'k':k,'fetch_k':fetch_k,'filter':filter} # See as_retriever docs for parameters
+        else:
+            search_kwargs={'k':k,'filter':filter} # See as_retriever docs for parameters
+
+        self.qa = ConversationalRetrievalChain(
+            retriever=self.vectorstore.as_retriever(search_type=search_type,
+                                                            search_kwargs=search_kwargs),
+            combine_docs_chain=self.doc_chain, 
+            question_generator=self.question_generator,
+            memory=self.chat_history,
+            verbose=verbose,
+            return_source_documents=True,
+            return_generated_question=True)
