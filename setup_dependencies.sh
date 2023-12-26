562ddbd1ce40eb72c5935b50918362d60a6f6e09
dsmueller3760 <dsm@danmueller.pro>
2023-12-26 15:16:47 -0500

    Update setup_dependencies.sh script

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,3 +1,5 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+!rm -rf .venv
+!python3 -m venv .venv
+!source .venv/bin/activate
+%pip install ipykernel datasets trl peft uuid pandas evaluate transformers bitsandbytes torch huggingface_hub accelerate ipywidgets scipy
+%pip freeze > requirements.txt
