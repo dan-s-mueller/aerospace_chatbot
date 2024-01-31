e7cf215330223545f72f134d36aa8552204e5f3f
dsmueller3760 <dsm@danmueller.pro>
2024-01-30 22:26:46 -0800

    Update chatbot description in Start.py

--- scripts/Start.py
+++ scripts/Start.py
@@ -10,21 +10,21 @@ st.markdown("Code base: https://github.com/dsmueller3760/aerospace_chatbot/tree/
 st.markdown('---')
 st.title("Chatbots")
 st.markdown("""
-Chatbots for aerospace mechanisms symposia, using all available papers published since 2000
-* Modular version meant to study retrieval methods
+This space contains chatbots and tools for exploring data in the aerospace mechanisms symposia, using all available papers published since 2000.
 """)
 st.subheader("Running Locally")
 '''
 It is recommended to run this streamlit app locally for improved performance. The hosted hugging face version is for proof of concept.
 You must have poetry installed locally to manage depdenencies. To run locally, clone the repository and run the following commands.
-```poetry config virtualenvs.in-project true
-```poetry install
-```source .venv/bin/activate
-```cd ./scripts
-```streamlit run Start.py
+    
+    poetry config virtualenvs.in-project true
+    poetry install
+    source .venv/bin/activate
+    cd ./scripts
+    streamlit run Start.py
 '''
 
-st.subheader("AMS")
+st.subheader("Aerospace Mechanisms Symposia (AMS)")
 '''
 This chatbot will look up from all Aerospace Mechanism Symposia in the following location: https://github.com/dsmueller3760/aerospace_chatbot/tree/main/data/AMS
 * Available models: https://platform.openai.com/docs/models
